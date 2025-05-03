a4394da6d85fe1fe157a5387882a4bff432d3526
SN-the-Jerry <myatpanpwintsoenyunt@gmail.com>
2025-05-03 23:32:57 +0700

    adding meta discription for seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,16 @@ import { IBM_Plex_Sans_Thai } from 'next/font/google';
 import ClientScripts from '../components/ClientScripts';
 import StylesheetLoader from '@/components/StylesheetLoader';
 
+
+import { metadata, viewport } from "./metadata";
 // Initialize the font
 const ibmPlexSansThai = IBM_Plex_Sans_Thai({
   weight: ['400', '500', '700'],
   subsets: ['thai', 'latin'],
   display: 'swap',
 });
+
+export { metadata, viewport };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-PG8P7JBS';
