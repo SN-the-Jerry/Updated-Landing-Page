687ff5a60acbb598a32d0c075a011222a8491501
Myat Pan Pwint Soe Nyunt ( SN ) <myatpanpwintsoenyunt@gmail.com>
2025-05-29 19:49:15 +0700

    HSTS headers added

--- next.config.mjs
+++ next.config.mjs
@@ -58,6 +58,10 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          },
+          {
             key: 'Cache-Control',
             value: 'public, max-age=31536000, immutable',
           },
